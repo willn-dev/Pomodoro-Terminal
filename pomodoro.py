8eb7ad2a2ce45873a7cfabdef29b7ad98af4ed1a
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-01-15 22:20:14 -0600

    got carraige return working

--- pomodoro.py
+++ pomodoro.py
@@ -5,9 +5,9 @@ def start():
     while total_seconds > 0: 
         mins = total_seconds // 60
         secs = total_seconds % 60
-        timer_display = f"{mins:02d}:{secs:02d}\n"
+        timer_display = f"\r{mins:02d}:{secs:02d}"
         time.sleep(1)
-        print(timer_display)
+        print(timer_display, end='', flush=True)
         total_seconds -= 1 
 
 if __name__ == ("__main__"):
