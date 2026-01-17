8781f6386cff0ca5843c03b0d1f08c493b465609
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-01-17 16:27:51 -0600

    figlet and liveupdate now functional

--- pomodoro.py
+++ pomodoro.py
@@ -1,20 +1,30 @@
 import time, os, sys
-from rich.console import Console
+from rich.live import Live
 from pyfiglet import Figlet
 
 
-console = Console
-b = Figlet(font="big")
 def start():
     total_seconds = 1500
-    while total_seconds > 0: 
-        mins = total_seconds // 60
-        secs = total_seconds % 60
-        timer_display = f"\r{mins:02d}:{secs:02d}"
-        time.sleep(1)
-        print (b.renderText(timer_display, end='', flush=True))
-        total_seconds -= 1 
-        
+
+    with Live(refresh_per_second=4) as live:
+        while total_seconds > 0: 
+            mins = total_seconds // 60
+            secs = total_seconds % 60
+
+            time.sleep(1)
+            live.update(format_time(mins, secs))
+
+            total_seconds -= 1 
+
+
+def format_time(mins, secs):
+
+    bigfig = Figlet(font="big")
+    timer_display = f"{mins:02d}:{secs:02d}"
+    return bigfig.renderText(timer_display)
+
+
+
 if __name__ == ("__main__"):
     start()
         
