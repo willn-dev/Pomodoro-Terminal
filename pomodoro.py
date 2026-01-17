9fb3e21e7e05710bd8b6b1f61c330829469d0d04
Will N <146034825+HelpItsAllGooberedUp@users.noreply.github.com>
2026-01-17 14:24:03 -0600

    new branch testing live displays

--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,10 @@
 import time, os, sys
+from rich.console import Console
+from pyfiglet import Figlet
 
+
+console = Console
+b = Figlet(font="big")
 def start():
     total_seconds = 1500
     while total_seconds > 0: 
@@ -7,9 +12,9 @@ def start():
         secs = total_seconds % 60
         timer_display = f"\r{mins:02d}:{secs:02d}"
         time.sleep(1)
-        print(timer_display, end='', flush=True)
+        print (b.renderText(timer_display, end='', flush=True))
         total_seconds -= 1 
-
+        
 if __name__ == ("__main__"):
     start()
         
